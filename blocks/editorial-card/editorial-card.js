4a5db359d1ff04f3bda83c60e8d2d08ce9a3dd97
Smruti Ranjan Mishra <smrutiranjan.m02@ad.infosys.com>
2026-09-11 16:12:56 +0530

    editorial cards

--- blocks/editorial-card/editorial-card.js
+++ blocks/editorial-card/editorial-card.js
@@ -11,18 +11,20 @@ export default function decorate(block) {
     rows[2]?.innerHTML || '';
 
   const primaryText =
-    rows[3]?.textContent.trim();
+    rows[3]?.textContent.trim() || '';
 
   const primaryUrl =
     rows[4]?.querySelector('a')?.getAttribute('href')
-    || rows[4]?.textContent.trim();
+    || rows[4]?.textContent.trim()
+    || '#';
 
   const secondaryText =
-    rows[5]?.textContent.trim();
+    rows[5]?.textContent.trim() || '';
 
   const secondaryUrl =
     rows[6]?.querySelector('a')?.getAttribute('href')
-    || rows[6]?.textContent.trim();
+    || rows[6]?.textContent.trim()
+    || '#';
 
   block.textContent = '';
 
@@ -31,23 +33,21 @@ export default function decorate(block) {
   const actions = [];
 
   if (
-    primaryText
-    && primaryText !== 'btn'
-    && primaryText !== '#'
+    primaryText&& primaryText 
   ) {
     actions.push(`
-      <a class="button primary" href="${t}
+       <a class="button secondary" href=${primaryUrl}>
+        ${primaryText}
       </a>
     `);
   }
 
   if (
-    secondaryText
-    && secondaryText !== 'btn'
-    && secondaryText !== '#'
+    secondaryText&& secondaryText 
   ) {
     actions.push(`
-      <a class="button secondary" href="${ext}
+      <a class="button secondary" href=${secondaryUrl}>
+        ${secondaryText}
       </a>
     `);
   }
@@ -61,15 +61,14 @@ export default function decorate(block) {
         ${text}
       </div>
 
-      ${
-        actions.length
-          ? `
-            <div class="editorial-card-actions">
+      ${actions.length
+      ? `
+            <div class="editorial-card-actions ">
               ${actions.join('')}
             </div>
           `
-          : ''
-      }
+      : ''
+    }
 
     </div>
   `;
