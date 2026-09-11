b1c4bdd341053e6cb639e942781f7c3b218610b2
Smruti Ranjan Mishra <smrutiranjan.m02@ad.infosys.com>
2026-09-11 13:38:01 +0530

    cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,22 +2,56 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
+
+    while (row.firstElementChild) {
+      li.append(row.firstElementChild);
+    }
+
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
+    // planning cards do not contain images
+    const hasImage = li.querySelector('.cards-card-image');
+
+    if (hasImage) {
+      li.classList.add('cards-destination');
+    } else {
+      li.classList.add('cards-planning');
+    }
+
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    const optimizedPic = createOptimizedPicture(
+      img.src,
+      img.alt,
+      false,
+      [{ width: '750' }],
+    );
+
+    moveInstrumentation(
+      img,
+      optimizedPic.querySelector('img'),
+    );
+
     img.closest('picture').replaceWith(optimizedPic);
   });
   block.replaceChildren(ul);
+  const section = block.closest('.section');
+
+  if (section?.classList.contains('dark')) {
+    block.classList.add('cards-planning');
+  } else {
+    block.classList.add('cards-destination');
+  }
 }
